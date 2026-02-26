98183230091e498e9123ef163e10f7fad1d5b04e
Michael <mi8rat@icloud.com>
2026-02-26 07:49:05 +0200

    first commit

--- mv-testimonials.php
+++ mv-testimonials.php
@@ -7,26 +7,10 @@
 * Version: 1.0
 * Requires at least: 5.6
 * Requires PHP: 7.0
-* Author: Marcelo Vieira
-* Author URI: https://www.codigowp.net
-* License: GPL v2 or later
-* License URI: https://www.gnu.org/licenses/gpl-2.0.html
+* Author: Michael
+* License: MIT
 * Text Domain: mv-testimonials
 * Domain Path: /languages
-*/
-/*
-MV Testimonials is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-any later version.
- 
-MV Testimonials is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
- 
-You should have received a copy of the GNU General Public License
-along with MV Testimonials. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
 */
 
 if ( ! defined( 'ABSPATH' ) ) {
